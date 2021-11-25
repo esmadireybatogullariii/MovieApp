548a0a0f1aa2a4144ddbde3d94479ef1e4f96019
Esmafer Direybatoğulları <esmaferdireybatogullari@gmail.com>
2021-11-26 02:34:14 +0300

    Constants object

---
+++ app/src/main/java/com/esma/movieapp/commons/Constants.kt
@@ -0,0 +1,10 @@
+package com.esma.movieapp.commons
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+    const val PARAM_MOVIE_ID = "movieId"
+    const val PARAM_TV_SHOW_ID = "showId"
+
+
+}
