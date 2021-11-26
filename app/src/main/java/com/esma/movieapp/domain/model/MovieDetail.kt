907d9bf45c7034be5a5863a4e1159c5b61f9a4e2
Esmafer Direybatoğulları <esmaferdireybatogullari@gmail.com>
2021-11-26 04:04:35 +0300

    toDetailHeader function for movie and tv show detail

--- app/src/main/java/com/esma/movieapp/domain/model/MovieDetail.kt
+++ app/src/main/java/com/esma/movieapp/domain/model/MovieDetail.kt
@@ -1,9 +1,19 @@
 package com.esma.movieapp.domain.model
 
-data class MovieDetail( val overview: String,
-                   val posterPath: String,
-                   val releaseDate: String,
-                   val title: String,
-                   val voteAverage: Double,
+data class MovieDetail(
+    val overview: String,
+    val posterPath: String,
+    val releaseDate: String,
+    val title: String,
+    val voteAverage: Double,
 
-)
+    )
+
+fun MovieDetail.toDetailHeader(): DetailHeader {
+    return DetailHeader(
+        title = title,
+        posterPath = posterPath,
+        voteAverage = voteAverage,
+        type = "Movie"
+    )
+}
--- app/src/main/java/com/esma/movieapp/domain/model/TvShowDetail.kt
+++ app/src/main/java/com/esma/movieapp/domain/model/TvShowDetail.kt
@@ -1,10 +1,20 @@
 package com.esma.movieapp.domain.model
 
-data class TvShowDetail(val firstAirDate: String,
-                        val name: String,
-                        val numberOfEpisodes: Int,
-                        val numberOfSeasons: Int,
-                        val overview: String,
-                        val posterPath: String,
-                        val voteAverage: Double)
+data class TvShowDetail(
+    val firstAirDate: String,
+    val name: String,
+    val numberOfEpisodes: Int,
+    val numberOfSeasons: Int,
+    val overview: String,
+    val posterPath: String,
+    val voteAverage: Double
+)
 
+fun TvShowDetail.toDetailHeader(): DetailHeader {
+    return DetailHeader(
+        title = name,
+        posterPath = posterPath,
+        voteAverage = voteAverage,
+        type = "TvShow"
+    )
+}
