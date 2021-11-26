d02d9f30285c5213547392f1479dce7523764d58
Esmafer Direybatoğulları <esmaferdireybatogullari@gmail.com>
2021-11-26 04:08:01 +0300

    Poster item is updated

--- app/src/main/java/com/esma/movieapp/presentation/home/components/PosterItem.kt
+++ app/src/main/java/com/esma/movieapp/presentation/home/components/PosterItem.kt
@@ -1,15 +1,12 @@
 package com.esma.movieapp.presentation.home.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.esma.movieapp.commons.Constants
@@ -22,15 +19,24 @@ fun PosterItem(
     onItemClick: (id: Int, type: String) -> Unit
 
 ) {
-    Column(modifier = Modifier
-        .height(225.dp)
-        .width(150.dp)
-        .clickable { onItemClick(id, type) }) {
-        Image(
-            painter = rememberImagePainter(Constants.BASE_IMAGE_URL + posterPath),
-            contentDescription = null,
-        )
+    Box(
+        Modifier
+            .padding(horizontal = 16.dp)
+            .wrapContentHeight()
+            .wrapContentWidth()
+    ) {
+        Box(modifier = Modifier
+            .height(225.dp)
+            .width(150.dp)
+            .clickable { onItemClick(id, type) }
+            .clip(RoundedCornerShape(16.dp, 16.dp, 16.dp, 16.dp))
+        ) {
+            Image(
+                painter = rememberImagePainter(Constants.BASE_IMAGE_URL + posterPath),
+                contentDescription = null
+            )
 
+        }
     }
 
 }
