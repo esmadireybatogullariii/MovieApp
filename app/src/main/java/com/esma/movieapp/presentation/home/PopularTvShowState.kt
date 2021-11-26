8a676b95d1a23328b890bea5c86e6aa0d99cd0b4
Esmafer Direybatoğulları <esmaferdireybatogullari@gmail.com>
2021-11-26 04:05:20 +0300

    Home states for popular movies and tv shows

--- app/src/main/java/com/esma/movieapp/presentation/home/PopularTvShowState.kt
+++ app/src/main/java/com/esma/movieapp/presentation/home/PopularTvShowState.kt
@@ -1,9 +1,6 @@
 package com.esma.movieapp.presentation.home
 
-import com.esma.movieapp.domain.model.LatestTvShow
-import com.esma.movieapp.domain.model.PopularMovie
 import com.esma.movieapp.domain.model.PopularTvShow
-import com.esma.movieapp.domain.model.TvShowDetail
 
 class PopularTvShowState(
     val isLoading: Boolean = false,
